ded1534f5f466f17eab18a672fc88c4d287b078a
CadenetVincent <CadenetVincent@users.noreply.github.com>
2019-10-21 17:20:52 +0200

    change admin & login & routes

--- src/Card/See_card.js
+++ src/Card/See_card.js
@@ -1,23 +1,48 @@
-  import React, { Component } from "react";
-  import {Row,Col} from 'react-bootstrap';
-
-  class See_card extends React.Component {
-
-    constructor(props)
-    {
-      super(props);
-    }
-
+import React, { Component } from "react";
+import {Row,Col} from 'react-bootstrap';
+import 'react-credit-cards/es/styles-compiled.css';
+import Cards from 'react-credit-cards';
+ 
+export default class See_card extends React.Component {
+  state = {
+    cvc: '',
+    expiry: '',
+    focus: '',
+    name: '',
+    number: '',
+  };
+ 
+  handleInputFocus = (e) => {
+    this.setState({ focus: e.target.name });
+  }
+  
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+    
+    this.setState({ [name]: value });
+  }
+  
   render() {
-
-
     return (
-      <div>
-      <h2 className="title_used"> Cards </h2>
-      </div>)
-
+      <div id="PaymentForm">
+        <Cards
+          cvc={this.state.cvc}
+          expiry={this.state.expiry}
+          focus={this.state.focus}
+          name={this.state.name}
+          number={this.state.number}
+        />
+        <form>
+          <input
+            type="tel"
+            name="number"
+            placeholder="Card Number"
+            onChange={this.handleInputChange}
+            onFocus={this.handleInputFocus}
+          />
+          ...
+        </form>
+      </div>
+    );
+  }
 }
-
-}
-
-export default See_card;
